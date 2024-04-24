fa4a2199cf5322c1f67e835258362f8860f75f71
Pedro Peres <php741@protonmail.com>
2024-04-24 15:33:30 -0300

    first commit

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+# Create your models here.
